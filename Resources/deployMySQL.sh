cb3c903c8268381d655f45ba41ec4182ef5317fc
Cybera Sysadmin <sysadmin@cybera.ca>
2016-11-22 12:21:05 -0700

    Review

--- Resources/deployMySQL.sh
+++ Resources/deployMySQL.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-vol=$(sudo readlink -e /dev/disk/by-uuid/$(sudo lsblk -o name,type,mountpoint,label,uuid | grep -v root | grep -v ephem | grep -v SWAP | grep -v vda | tail -1 | awk '{print $3}'))
+vol='/dev/'$(sudo lsblk -o name,type,mountpoint,label,uuid | grep -v root | grep -v ephem | grep -v SWAP | grep -v vda | tail -1 |awk '{print $1}')
 
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password %ROOT_MYSQL_PASSWORD%'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password %ROOT_MYSQL_PASSWORD%'
 
-sudo mkfs -t ext3 $vol
+sudo mkfs -t ext4 $vol
 sudo mkdir /opt/mysql_data
 sudo mount $vol /opt/mysql_data
 
 sudo apt-get -y -q install mysql-server
 sudo service mysql stop
 
-sudo cp -r /var/lib/mysql/ /opt/mysql_data/
-sudo rm -r /var/lib/mysql/
+sudo cp -a /var/lib/mysql/ /opt/mysql_data/
+sudo rm -rf /var/lib/mysql/
 sudo ln -s /opt/mysql_data/mysql /var/lib/mysql
 
-echo "$vol /opt/mysql_data ext3 defaults 0 1 "| sudo tee --append  /etc/fstab
+echo "$vol /opt/mysql_data ext4 defaults 0 1 "| sudo tee --append  /etc/fstab
 sudo chown -R mysql:mysql /opt/mysql_data/mysql
 
 echo "/opt/mysql_data/mysql/ r,
 /opt/mysql_data/mysql/** rwk," | sudo tee --append /etc/apparmor.d/local/usr.sbin.mysqld
 
-sudo service mysql start
+sudo service mysql restart
 
 
 mysql --user=root --password=%ROOT_MYSQL_PASSWORD% -e "CREATE DATABASE wordpress"
